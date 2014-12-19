ddd3c4a1427484bc5de26cdc87be80eca39e32c4
Q.M. Ashfaq Ali <Q.M.AshfaqAli@gmail.com>
2014-12-19 14:50:04 +0600

    Splashscreen
    Added splash screen with sound

---
+++ gen/com/avocado/contactbook/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.avocado.contactbook;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/avocado/contactbook/Splashscreen.java
@@ -0,0 +1,30 @@
+package com.avocado.contactbook;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashscreen extends Activity{
+	public static final int SPLASH_TIME_OUT = 5000;
+	MediaPlayer mp;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splashscreen);
+		
+		mp = MediaPlayer.create(getBaseContext(), R.raw.startup);
+		mp.start();
+		
+		new Handler().postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		}, SPLASH_TIME_OUT);
+	}
+}
